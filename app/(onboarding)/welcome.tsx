d07de6f77eaed6bb64a48ae51a74c7ebd5ca9a5b
yari-dewalt <yari.dewalt@gmail.com>
2025-09-09 14:38:03 -0700

    chore: update app screenshots

--- app/(onboarding)/welcome.tsx
+++ app/(onboarding)/welcome.tsx
@@ -41,7 +41,7 @@ const previewData = [
   {
     id: '4',
     backgroundImage: 'https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-    screenshot: require('../../assets/screenshots/workout_details_3.png'),
+    screenshot: require('../../assets/screenshots/workout_details_2.png'),
     text: 'Track your progress'
   },
 ];
